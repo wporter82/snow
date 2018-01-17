03a128509abaf321a119ca6e7ae85670b1608b69
Wayne Porter <wporter82@gmail.com>
2018-01-17 15:01:26 -0800

    New linked list functions
    Signed-off-by: Wayne Porter <wporter82@gmail.com>

--- snow.c
+++ snow.c
@@ -9,14 +9,16 @@
 #define MAX_FLAKES_PER_TICK 5
 
 // Linked list of flakes
-typedef struct _flake {
+typedef struct flake {
 	int x;
 	int y;
 	int spriteindex;
 	int age;
-	struct _flake * next;
-	struct _flake * prev;
+	struct flake* next;
 } flake;
+
+// Callback to manipulate nodes of the linked list
+typedef void (*callback)(flake* data);
 
 // Unicode characters to use as sprites
 const wchar_t* sprite[8] = {
@@ -32,77 +34,151 @@ const wchar_t* sprite[8] = {
 
 const int num_sprites = sizeof(sprite) / sizeof(sprite[0]);
 
-void append_flake(flake * head, int x, int y, int spriteindex, int age) {
-	flake * current = head;
+flake* create(int x, int y, int spriteindex, int age, flake* next) {
+	flake* new_flake = (flake*)malloc(sizeof(flake));
+	if(new_flake == NULL) {
+		printf("Error creating a new flake.\n");
+		exit(0);
+	}
+	new_flake->x = x;
+	new_flake->y = y;
+	new_flake->spriteindex = spriteindex;
+	new_flake->age = age;
+	new_flake->next = next;
+
+	return new_flake;
+}
+
+flake* prepend(flake* head, int x, int y, int spriteindex, int age) {
+	flake* new_flake = create(x,y,spriteindex,age,head);
+	head = new_flake;
+	return head;
+}
+
+flake* append(flake* head, int x, int y, int spriteindex, int age) {
 	/* Get to the end of the list */
-	while(current->next != NULL) {
-		current = current->next;
+	flake* cursor = head;
+	while(cursor->next != NULL) {
+		cursor = cursor->next;
 	}
 
-	current->next = malloc(sizeof(flake));
-	current->next->x = x;
-	current->next->y = y;
-	current->next->spriteindex = spriteindex;
-	current->next->age = age;
-	current->next->next = NULL;
-	current->next->prev = current;
+	/* Create a new flake */
+	flake* new_flake = create(x,y,spriteindex,age,NULL);
+	cursor->next = new_flake;
+
+	return head;
 }
 
-void draw_flakes(flake * head) {
-	flake * current = head;
-	while(current->next != NULL) {
-		mvaddwstr(current->y, current->x, sprite[current->spriteindex]);
-		current = current->next;
+flake* remove_front(flake* head) {
+	if(head == NULL)
+		return NULL;
+	flake *front = head;
+	head = head->next;
+	/* check if last in list */
+	if(front == head)
+		head = NULL;
+	free(front);
+	return head;
+}
+
+flake* remove_back(flake* head) {
+	if(head == NULL)
+		return NULL;
+
+	flake *cursor = head;
+	flake *back = NULL;
+	while(cursor->next != NULL) {
+		back = cursor;
+		cursor = cursor->next;
 	}
+	if(back != NULL)
+		back->next = NULL;
+
+	/* check if last in list */
+	if(cursor == head)
+		head = NULL;
+
+	free(cursor);
+	return head;
 }
 
-void update_flakes(flake *head) {
-	if(head == NULL) return;
+flake* remove_any(flake* head, flake* node) {
+	/* if first in list */
+	if(node == head){
+		head = remove_front(head);
+		return head;
+	}
 
-	/* Move the flake and update it's age */
-	flake * current = NULL;
-	while((current = head) != NULL) {
-		if(head->age > 0) {
-			head->age--;
-			head->y++;
+	/* if last in list */
+	if(node->next == NULL) {
+		head = remove_back(head);
+		return head;
+	}
+
+	/* if in the middle */
+	flake* cursor = head;
+	while(cursor != NULL) {
+		if(cursor->next == node) {
+			break;
 		}
-		head = head->next;
+		cursor = cursor->next;
+	}
+
+	if(cursor != NULL) {
+		flake* tmp = cursor->next;
+		cursor->next = tmp->next;
+		tmp->next = NULL;
+		free(tmp);
+	}
+
+	return head;
+}
+
+void dispose(flake *head) {
+	flake *cursor, *tmp;
+
+	if(head != NULL) {
+		cursor = head->next;
+		head->next = NULL;
+		while(cursor != NULL) {
+			tmp = cursor->next;
+			free(cursor);
+			cursor = tmp;
+		}
 	}
 }
 
-void remove_dead(flake ** head) {
-	flake * current = *head;
+void traverse(flake* head, callback f) {
+	flake* cursor = head;
+	while(cursor != NULL) {
+		f(cursor);
+		cursor = cursor->next;
+	}
+}
 
-	while(current->next != NULL) {
-		if(current->age < 1) {
-			flake * temp = NULL;
-			temp = current;
-
-			if(temp->prev != NULL) {
-				temp->prev->next = temp->next;
-			} else {
-				temp->next->prev = NULL;
-				/* Set the new head of the list */
-				*head = temp->next;
-			}
-
-			current = temp->next;
-			free(temp);
+flake* remove_dead(flake* head) {
+	flake* cursor = head;
+	flake* tmp = NULL;
+	while(cursor != NULL) {
+		if(cursor->age <= 0) {
+			tmp = cursor->next;
+			head = remove_any(head, cursor);
+			cursor = tmp;
 		} else {
-			current = current->next;
+			cursor = cursor->next;
 		}
 	}
+
+	return head;
 }
 
-int count_list(flake *head) {
-	flake *current = NULL;
+int count(flake* head) {
+	flake* cursor = head;
 	int count = 0;
-
-	while((current = head) != NULL) {
+	while(cursor != NULL) {
 		count++;
-		head = head->next;
+		cursor = cursor->next;
 	}
-
 	return count;
 }
 
@@ -116,13 +192,33 @@ void cleanup(flake *head) {
 
 }
 
+void draw_flake(flake* fl) {
+	if(fl != NULL)
+		mvaddwstr(fl->y, fl->x, sprite[fl->spriteindex]);
+		 
+}
+
+void update_flake(flake* fl) {
+	if(fl != NULL) {
+		/* move the flake */
+		if(fl->age > 0) {
+			fl->age--;
+			fl->y++;
+		}
+	}
+}
+
+
 int main(void)
 {
 	int maxy;
 	int maxx;
+
 	int flake_count = 0;
 	int flakes_to_make = 0;
-	flake * head = NULL;
+	flake* head = NULL;
+	callback draw = draw_flake;
+	callback update = update_flake;
 
 	// Init curses
 	setlocale(LC_ALL, ""); /* Enable using all unicode characters */
@@ -136,8 +232,9 @@ int main(void)
 	getmaxyx(stdscr, maxy, maxx);
 
 	/* Init linked list of flakes with a single one to start with */
-	head = malloc(sizeof(flake));
+	head = (flake*)malloc(sizeof(flake));
 	if(head == NULL) return 1;
+	head = create(0,0,0,maxy,NULL);
 
 	// Main animation loop that exits when ESC is pressed
 	while(getch() != 27)
@@ -146,9 +243,9 @@ int main(void)
 		getmaxyx(stdscr, maxy, maxx);
 
 		/* Move existing flakes */
-		update_flakes(head);
-		remove_dead(&head);
-		flake_count = count_list(head);
+		traverse(head, update);
+		head = remove_dead(head);
+		flake_count = count(head);
 
 		/* Generate some new flakes if we aren't at the max */
 		if(flake_count < MAX_FLAKES) {
@@ -165,12 +262,12 @@ int main(void)
 				/* Get random sprite */
 				int spritenum = rand() / (RAND_MAX / num_sprites + 1);
 				/* Add flake to the list */
-				append_flake(head, randx, 0, spritenum, maxy);
+				head = create(randx, 0, spritenum, maxy, head);
 			}
 		}
 
 		clear();
-		draw_flakes(head);
+		traverse(head, draw);
 		refresh();
 	}
 
